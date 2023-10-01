79e96d4663533ceac3f8f5220d6aa63f16f7f46a
Akanksha872 <akankshasingh872@gmail.com>
2023-10-02 03:26:08 +0530

    added pagination

--- book-my-sport/src/components/event/EventList.js
+++ book-my-sport/src/components/event/EventList.js
@@ -1,22 +1,39 @@
-import { Typography } from '@mui/material';
+import { Pagination, Stack, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import EventCard from './EventCard';
 
 function EventList ({ eventsList, allEvents, updateEvents, registeredEventIds }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5; 
+  const noOfPages = Math.ceil(eventsList.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  let eventsToDisplay = eventsList.slice(startIndex, endIndex);
+
+  const handlePageChange = (event, newPage) => {
+    setCurrentPage(newPage);
+  };
   
   if(!eventsList.length){
     return <Typography variant="h6" color="inherit" align="center" mt={2} mb={4}>No Data Available</Typography>
   }
   
   return (
-    <Grid container spacing={2}>
-      {eventsList.map((event) => (
-        <Grid item key={event.id} xs={12} sm={6} md={5} lg={4}>
-          <EventCard event={event} allEvents={allEvents}  updateEvents={updateEvents}  registeredEventIds={registeredEventIds}/>
-        </Grid>
-      ))}
-    </Grid>
+    <>
+      <Grid container spacing={2}>
+        {eventsToDisplay.map((event) => (
+          <Grid item key={event.id} xs={12} sm={6} md={5} lg={4}>
+            <EventCard event={event} allEvents={allEvents}  updateEvents={updateEvents}  registeredEventIds={registeredEventIds}/>
+          </Grid>
+        ))}
+      </Grid>
+      {noOfPages != 1 && <Stack spacing={2} alignItems="center" mt={4}>
+        <Pagination count={noOfPages} color="primary" page={currentPage}   onChange={handlePageChange}/>
+      </Stack>}
+    </>
   );
 }
 
